d34183585b5a5a66607d3c461ce8a0e7c9c3abaa
DelisaRogger <delrog47231@stud.noroff.no>
2024-09-15 17:03:23 +0200

    pretty

--- js/sort.js
+++ js/sort.js
@@ -1,35 +1,33 @@
 import { post } from "./post.js";
 
-export function sortAlphabetic(value1, value2){
-    post.sort((a, b) => {
-        const titleA = a.acf.title.toUpperCase();
-        const titleB = b.acf.title.toUpperCase();
-        if (titleA < titleB){
-            return value1;
-        }
-    
-        if (titleA > titleB){
-            return value2;
-        }
-    
-        return 0;
-    })    
-}
-
-export function sortByDate(value1, value2){
-post.sort(function(a,b){
-    const date1 = new Date(a.date);
-    const date2 = new Date(b.date);
-    if(date1 < date2){
-        return value1;
+export function sortAlphabetic(value1, value2) {
+  post.sort((a, b) => {
+    const titleA = a.acf.title.toUpperCase();
+    const titleB = b.acf.title.toUpperCase();
+    if (titleA < titleB) {
+      return value1;
     }
 
-        if(date1 > date2){
-            return value2;
-        }
+    if (titleA > titleB) {
+      return value2;
+    }
 
-        return 0; 
+    return 0;
   });
 }
 
+export function sortByDate(value1, value2) {
+  post.sort(function (a, b) {
+    const date1 = new Date(a.date);
+    const date2 = new Date(b.date);
+    if (date1 < date2) {
+      return value1;
+    }
 
+    if (date1 > date2) {
+      return value2;
+    }
+
+    return 0;
+  });
+}
